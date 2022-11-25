ad46aa796f70d225a8769ccecb61488a782e7b55
Hitory <hitory@yahoo.com>
2022-11-25 10:21:09 +0300

    | Lecture 3. | 25.11.2022 | 10:21 |

---
+++ src/main/java/Java_First_Level_Lecture_3/App.java
@@ -0,0 +1,51 @@
+package Java_First_Level_Lecture_3;
+
+public class App {
+
+    public static String name = "Vasya";
+
+    public static void main(String[] args) {
+
+        System.out.println(method1(true) & method2(false));
+
+
+        if (name != null && name.length() > 2) {
+            System.out.println(name);
+        }
+
+        // Исключающее или
+        System.out.println(true ^ false);
+
+        // Побитовый сдвиг (можно двтгать влево вправо причём без учёта знака)
+        int i = 0b001100;
+        System.out.println(i);
+        System.out.println(i = i << 2);
+        // Также валидно
+        System.out.println(i <<= 2);
+        // С помощью класса обёртки выводим целочисленную переменную i в бинарном представлении
+        System.out.println(Integer.toBinaryString(i));
+        // Отрицательное число в двоичной форме - инвертированное число + 1
+        System.out.println();
+        int n = 2;
+        System.out.println(Integer.toBinaryString(n));
+        System.out.println(Integer.toBinaryString(~n));
+        System.out.println(~n);
+        // Что бы из 2 сделать -2 - необходимо проинвертировать (~) и прибавить 1
+        System.out.println(~n+1);
+        System.out.println();
+        // Побитовое "и" и побитовое "или"
+        System.out.println(2 | 4);
+        System.out.println(Integer.toBinaryString(2));
+        System.out.println(Integer.toBinaryString(4));
+    }
+
+    private static boolean method1(boolean b) {
+        System.out.println("method1(" + b + ")");
+        return b;
+    }
+
+    private static boolean method2(boolean b) {
+        System.out.println("method2(" + b + ")");
+        return b;
+    }
+}
---
+++ src/main/java/Java_First_Level_Lecture_3/Elevator.java
@@ -0,0 +1,37 @@
+package Java_First_Level_Lecture_3;
+
+public class Elevator {
+
+    public boolean doorOpen = false; // Двери лифта по умолчанию закрыты
+    int currentFloor = 1; // Все лифыт по умолчанию стоят
+    public final int TOP_FLOOR = 10;
+    public final int MIN_FLOORS = 1;
+
+    public void openDoor() {
+        System.out.println("Opening door.");
+        doorOpen = true;
+        System.out.println("Door is open.");
+    }
+
+    public void closeDoor() {
+        System.out.println("Closing door.");
+        doorOpen = false;
+        System.out.println("Door is closed");
+    }
+
+    public void goUp() {
+        System.out.println("Going up one floor.");
+        currentFloor++;
+        System.out.println("Floot: " + currentFloor);
+    }
+
+    public void goDown() {
+        System.out.println("Going down one floor.");
+        currentFloor--;
+        System.out.println("Floot: " + currentFloor);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
