61defbfba7d612ff80c415b5460eaa0e9d22758b
Hitory <hitory@yahoo.com>
2022-12-01 10:14:52 +0300

    | Lecture 7. | 01.12.2022 | 10:14 |

--- src/main/java/Java_First_Level_Lecture_6/LibraryApp.java
+++ src/main/java/Java_First_Level_Lecture_6/LibraryApp.java
@@ -1,11 +1,12 @@
 package Java_First_Level_Lecture_6;
 
 import java.awt.print.Book;
+import java.io.IOException;
 import java.util.Random;
 
 public class LibraryApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         int libSize = 32;
         Library karaskevich = new Library("Pushkin", libSize);
@@ -19,6 +20,16 @@ public class LibraryApp {
         karaskevich.booksOderThen(1950);
 
         //System.out.println(karaskevich);
+        System.out.println("|---------------------------|");
 
+        int libsize1 = 3;
+        Library library = new Library("Karaskevich", libsize1);
+
+
+        for (int i = 0; i < libsize1; i++) {
+            library.putBook();
+        }
+
+        System.out.println(library);
     }
 }
